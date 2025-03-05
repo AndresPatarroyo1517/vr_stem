09464d9545a7e53501da6cb356ea63d51fd2f833
AndresPatarroyo1517 <munozandres115@gmail.com>
2025-03-04 22:32:42 -0500

    Teleport script en los componentes floor

--- Assets/_Barking_Dog/Common Scripts/Turn_Move.cs
+++ Assets/_Barking_Dog/Common Scripts/Turn_Move.cs
@@ -1,31 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Turn_Move : MonoBehaviour {
-	public int TurnX;
-	public int TurnY;
-	public int TurnZ;
+    public int TurnX;
+    public int TurnY;
+    public int TurnZ;
 
-	public int MoveX;
-	public int MoveY;
-	public int MoveZ;
+    public int MoveX;
+    public int MoveY;
+    public int MoveZ;
 
-	public bool World;
+    public bool World;
+    
+    // Variable para activar la animación
+    private bool shouldAnimate = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (World == true) {
-			transform.Rotate(TurnX * Time.deltaTime,TurnY * Time.deltaTime,TurnZ * Time.deltaTime, Space.World);
-			transform.Translate(MoveX * Time.deltaTime, MoveY * Time.deltaTime, MoveZ * Time.deltaTime, Space.World);
-		}else{
-			transform.Rotate(TurnX * Time.deltaTime,TurnY * Time.deltaTime,TurnZ * Time.deltaTime, Space.Self);
-			transform.Translate(MoveX * Time.deltaTime, MoveY * Time.deltaTime, MoveZ * Time.deltaTime, Space.Self);
-		}
-	}
+    // Se activa la animación mediante el trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            shouldAnimate = true;
+        }
+    }
+
+    // Update se ejecuta cada frame
+    void Update () {
+        if (shouldAnimate) {
+            if (World) {
+                transform.Rotate(TurnX * Time.deltaTime, TurnY * Time.deltaTime, TurnZ * Time.deltaTime, Space.World);
+                transform.Translate(MoveX * Time.deltaTime, MoveY * Time.deltaTime, MoveZ * Time.deltaTime, Space.World);
+            } else {
+                transform.Rotate(TurnX * Time.deltaTime, TurnY * Time.deltaTime, TurnZ * Time.deltaTime, Space.Self);
+                transform.Translate(MoveX * Time.deltaTime, MoveY * Time.deltaTime, MoveZ * Time.deltaTime, Space.Self);
+            }
+        }
+    }
 }
